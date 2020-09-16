d01827be804178f8e7a60036d3652db3e180c8f8
kasumi64 <77638292@qq.com>
2020-09-16 20:10:55 +0800

    新增直接调用的vue-ui库；

---
+++ src/libs/package/package.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+
+// import './element-ui/theme-chalk^2.13.2.css';
+// import ElementUI from './element-ui/index^2.13.2.js';
+// Vue.use(ElementUI);
+
+// import './vuetify/vuetify-v2.3.10.css';
+// import Vuetify from './vuetify/vuetify-v2.3.10.min.js';
+// Vue.use(Vuetify);
+
+import './heyui/heyui-default.css';
+// import './heyui/heyui-lavender.css';
+import heyui from './heyui/heyui^1.26.1.js';
+Vue.use(heyui);
+
+// import './vant/vant^2.10.6.css';
+// import Vant from './vant/vant^2.10.6.min.js';
+// Vue.use(Vant);
+
+// import './iView/iview^4.3.2.css';
+// import iView from './iView/iview^4.3.2.min.js';
+// Vue.use(iView);
+
+// import './ant-design/antd-v1.6.5.css';
+// import antd from './ant-design/antd-v1.6.5.js';
+// Vue.use(antd);
+
+import '@libs/style.scss'
+
+// import '@libs/svgLoad.js'
